eb248b33080ca22909d94ca8b181270987b49711
Senam-Kudjo <senamhumado56@gmail.com>
2023-03-31 16:47:12 +0000

    3d portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import Hero from "./components/Hero";
+import { About } from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover">
+          <Navbar />
+          <Hero />
+          <About />
+          <Navbar />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
